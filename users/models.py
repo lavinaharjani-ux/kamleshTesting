eb3d103bf8d860bdf5cbdc8e3636734315f9988b
Abhi8099 <abhishek.sharma1@digranknow.com>
2026-05-28 14:39:50 +0530

    subject name module create

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
