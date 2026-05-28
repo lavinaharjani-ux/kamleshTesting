32ddb4f6c4b6683083c16e498f3284fcadb907f5
Abhi8099 <abhishek.sharma1@digranknow.com>
2026-05-28 15:02:56 +0530

    subject migarrtin also add in one branch

--- users/models.py
+++ users/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
+    date = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, default="jaipur")
+    address_1 = models.CharField(max_length=255, default="jaipur")
+    city = models.CharField(max_length=100, default="mp")
+
 
     def __str__(self):
         return self.name
