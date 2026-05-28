b767bb1cd35ae0493cc2e899e96d59a8b9e9b915
Abhi8099 <abhishek.sharma1@digranknow.com>
2026-05-28 14:36:55 +0530

    migration file add

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
