9dadee7aed46f4b86a9dcf6bd2c018d59f4c49e5
Abhi8099 <abhishek.sharma1@digranknow.com>
2026-06-18 13:06:42 +0530

    testing

--- demo/urls.py
+++ demo/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+
+from django.http import JsonResponse
+
+def worker_api(request):
+    data = {
+        "status": True,
+        "message": "Worker API is working successfully",
+        "name": "Kamlesh"
+    }
+    return JsonResponse(data, status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', worker_api, name='worker_api'),
 ]
